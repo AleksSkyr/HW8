c255c672dc951a20a2430a5298ce13a61efcf5de
AleksSkyr <greshnik1499@gmail.com>
2023-05-10 15:28:20 +0300

    start

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] weights = new int[5];
+        weights[0] = 1;
+        weights[1] = 2;
+        weights[2] = 3;
+        for (int i = 0; i < 12; i++) {
+            System.out.println(weights[i]);
+        }
+
+    }
+}
